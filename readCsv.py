44758bb498bdb060025f9ae67a6cdf839d988467
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-04-25 22:49:53 +0800

    updates

--- readCsv.py
+++ readCsv.py
@@ -20,8 +20,26 @@ with open(os.path.join(dirname, 'edges.csv')) as edges_csv:
     for idx, row in enumerate(csv_reader):
         if idx != 0:
             edge_tuple_arr.append((row[0], row[1], row[2]))
-            
-print(edge_tuple_arr)
+
+with open(os.path.join(dirname, 'edges2.csv')) as edges2_csv:
+    csv_reader2 = csv.reader(edges2_csv, delimiter='\t')
+    vertical_roads = []
+    horizontal_roads = []
+    ordered_pairs = {}
+    unordered_pairs_list = []
+    for idx, row in enumerate(csv_reader2):
+        horizontal_road_label = row[0]
+        ordered_pairs.setdefault(horizontal_road_label,[])
+        for idx2, column in enumerate(row):
+            if idx == 0:
+                vertical_roads.append(column.strip())
+            if idx != 0 and idx2 > 0:
+                if column == 'x':
+                    ordered_pairs[horizontal_road_label].append(vertical_roads[idx2])
+                    unordered_pairs_list.append((horizontal_road_label, vertical_roads[idx2]))
+    print(vertical_roads)
+
+# print(edge_tuple_arr)
 # print(json.dumps(node_dict,sort_keys=True, indent=4))
 
 # edges = [('A', 'B', 2), ('A', 'C', 5), ('B', 'D', 3), ('B', 'E', 7),
