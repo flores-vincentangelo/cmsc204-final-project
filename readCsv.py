2d16f751dc4b86e181b95c919bef6b40ad3385f4
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-04-29 20:19:40 +0800

    read csv

--- readCsv.py
+++ readCsv.py
@@ -21,22 +21,34 @@ with open(os.path.join(dirname, 'edges.csv')) as edges_csv:
         if idx != 0:
             edge_tuple_arr.append((row[0], row[1], row[2]))
 
+vertical_roads = []
+horizontal_roads = []
+row_values = []
 with open(os.path.join(dirname, 'edges2.csv')) as edges2_csv:
     csv_reader2 = csv.reader(edges2_csv, delimiter=',')
-    vertical_roads = []
-    horizontal_roads = []
     unordered_pairs_list = []
     for idx, row in enumerate(csv_reader2):
+        row_values.append([])
         for idx2, column_val in enumerate(row):
             if idx == 0:
                 vertical_roads.append(column_val)
             
             if idx2 == 0:
                 horizontal_roads.append(column_val)
-            elif idx2 > 0:
-                if column_val == 'x':
-                    unordered_pairs_list.append((horizontal_roads[idx], vertical_roads[idx2]))
+                row_values[idx].append(column_val)
+                continue
+            if column_val == 'x':
+                row_values[idx].append(f'{horizontal_roads[idx]}-{vertical_roads[idx2]}')
+                unordered_pairs_list.append((horizontal_roads[idx], vertical_roads[idx2]))
+            else:
+                row_values[idx].append('')
     print(vertical_roads)
+
+with open(os.path.join(dirname, 'nodes2.csv'), 'w') as write_csv_file:
+    writer = csv.writer(write_csv_file)
+    writer.writerow(vertical_roads)
+    writer.writerows(row_values)
+    
 
 # print(edge_tuple_arr)
 # print(json.dumps(node_dict,sort_keys=True, indent=4))
